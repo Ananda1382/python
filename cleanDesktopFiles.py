ae2591e7376507c661c4e0b2fee9cd046f729e4e
Ananda Chandran <achand27@ford.com>
2024-06-06 16:53:45 -0400

    Program to clean the desktop files

--- cleanDesktopFiles.py
+++ cleanDesktopFiles.py
@@ -1,25 +1,32 @@
 import os
+import shutil
+
+folders = {
+    "ImageFiles": ['.jpg', '.png', '.jpeg','.gif'],
+    "PdfFiles": ['.pdf'],
+    "WordFiles": ['.doc', '.docx', '.docs'],
+    "ExcelFiles": ['.xlsx']
+}
 
 folder_original = 'C:/Users/ACHAND27/Desktop/'
 folder_destination = 'C:/Users/ACHAND27/Desktop/CleanedUp'
-pdf_folder = 'C:/Users/ACHAND27/Desktop/CleanedUp/PdfFiles'
-word_folder = 'C:/Users/ACHAND27/Desktop/CleanedUp/WordFiles/'
-image_folder = 'C:/Users/ACHAND27/Desktop/CleanedUp/ImageFiles/'
 
-def create_Folder(folder):
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-
-def move_files(folder_original, folder_target, entry):
-    create_Folder(folder_target)
-    location_original = os.path.join(folder_original, entry.name)
-    location_destination = os.path.join(folder_target, entry.name)
-    os.rename(location_original, location_destination)
-
-for entry in os.scandir(folder_original):
-    if os.path.isfile(entry) & entry.name.endswith('.pdf'):        
-        move_files(folder_original, pdf_folder, entry)        
-    elif os.path.isfile(entry) & entry.name.endswith(('.doc', '.docx', '.docs')):
-        move_files(folder_original, word_folder, entry)
-    elif os.path.isfile(entry) & entry.name.lower().endswith(('.jpg', '.png', '.jpeg','.gif')):
-        move_files(folder_original, image_folder, entry)    
+#created the folders
+def create_Folder():
+    for folder in folders:
+        folder_path = os.path.join(folder_destination, folder)
+        print("Folder path :",folder_path)
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+#Move the files into corresponding folder
+for file_name in os.listdir(folder_original):
+    original_file_path = os.path.join(folder_original, file_name)
+    if os.path.isfile(original_file_path):        
+        for folder_name, extentions in folders.items():
+            for extention in extentions:
+                if file_name.endswith(extention):
+                    destination_Sub_folder = os.path.join(folder_destination, folder_name)
+                    print("original_file_path:", original_file_path, ",Destination Folder:", destination_Sub_folder)
+                    create_Folder()
+                    shutil.move(original_file_path, destination_Sub_folder)
+                    
